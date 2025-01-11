0a360277dea2d258956d8b436fc17be9ccc6bce5
GAP-Pathum <pathumpasindu41@gmail.com>
2025-01-11 13:59:29 +0530

    fixed issues in importings

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "tailwindcss/tailwind.css";
 import Loading from "./components/Loading"; // Ensure correct path
 import Landing from "./pages/Landing";
-import Myself from "./components/myself";
+import Myself from "./components/Myself";
 import Education from "./components/Education";
 import Skills from "./components/Skills/Skills";
 import ParticlesBackground from "./components/ParticlesBackground";
@@ -24,7 +24,7 @@ const App = () => {
 
     return (
         <div className="relative bg-black min-h-screen overflow-hidden">
-            {/* <ParticlesBackground /> */}
+            <ParticlesBackground />
             <div className="relative z-10">
                 <Landing />
                 <Myself />
