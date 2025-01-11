7a5098cf52b951200c842d99e85dce8e9775a0d4
Pasindu Pathum <pathumpasindu41@gmail.com>
2025-01-11 14:02:35 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import "tailwindcss/tailwind.css";
 import Loading from "./components/Loading"; // Ensure correct path
 import Landing from "./pages/Landing";
-import Myself from "./components/myself";
+import Myself from "./components/Myself";
 import Education from "./components/Education";
 import Skills from "./components/Skills/Skills";
 import ParticlesBackground from "./components/ParticlesBackground";
